1768e064776226c5cad22ed0d661161f49152129
Deaths-Door <aaravaditya51@gmail.com>
2024-08-11 11:16:40 +0200

    Custom Failure Example Works

--- examples/custom_failure_manager.rs
+++ examples/custom_failure_manager.rs
@@ -1,28 +1,61 @@
 use bevy::prelude::*;
-use bevy_intro_screen::prelude::*;
-
-fn main() {
-    let transition_to = AppState::Menu;
-    let preferences = IntroPreferences::builder()
-        .run_at(AppState::IntroScreen)
-        .transition_to(transition_to)
-        .skip_on_input(true)
-        .duration(FixedDuration::new(transition_to))
-        .ui(GameIntroScreen)
-        .build();
-
-    let splash_plugin = IntroScreenPlugin::builder()
-        .preferences(preferences)
-        // Use multiple failure stragies together
-        .failure_manager(LogFailure.and(OnFailureCloseWindowWithDelay(
-            std::time::Duration::from_secs(3),
-        )))
-        .build();
-
+use bevy_intro_screen::prelude::{*,egui::EguiIntroScreen};
+use bevy_egui::EguiContexts;
+use std::time::Duration;
+fn main() {    
     App::new()
-        .add_plugins((DefaultPlugins, splash_plugin))
+        .add_plugins(AppPlugin)
         .run();
 }
+
+const APP_NAME: &'static str = "My game";
+
+const LABEL: &'static str = "EGUI INTRO SCREEN";
+
+pub struct AppPlugin;
+impl Plugin for AppPlugin {
+    fn build(&self,app: &mut App) {
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: String::from(APP_NAME),
+                ..Default::default()
+            }),
+            close_when_requested: true,
+            ..Default::default()
+        }));
+
+        app.add_systems(Startup, setup);
+
+        app.init_state::<AppState>();
+
+        let transition_to = AppState::GameMenu;
+        let preferences = IntroPreferences::builder()
+            .run_at(AppState::SplashScreen)
+            .transition_to(transition_to)
+            .skip_on_input(true)
+            .duration(FixedDuration::new_with_duration(
+                Duration::from_millis(5000),
+                transition_to,
+            ))
+            .ui(GameIntroScreen)
+            .build();
+
+        let intro_plugin = IntroScreenPlugin::builder()
+            .preferences(preferences)
+            // Main Difference
+            .failure_manager(LogFailure.and(OnFailureCloseWindow))
+            .build();
+
+        app.add_plugins(intro_plugin);
+
+        app.add_systems(OnEnter(IntroState::Running),to_failure);
+    }
+}
+
+fn to_failure(mut next_state : ResMut<NextState<IntroState>>) {
+    next_state.set(IntroState::Failure)
+}
+
 
 #[derive(Clone)]
 pub struct LogFailure;
@@ -41,15 +74,13 @@ fn log(_: Commands) {
     eprintln!("the game has failed!!!1");
 }
 
-/// Same ----
-#[derive(States, Clone, PartialEq, Eq, Hash, Debug, Copy)]
-pub enum AppState {
-    IntroScreen,
-    Menu,
-}
 
-#[derive(Clone)]
-pub struct GameIntroScreen;
+// Same as EGUI example
+
+fn setup(contexts: EguiContexts,mut commands : Commands) {
+    egui_extras::install_image_loaders(contexts.ctx());
+    commands.spawn(Camera2dBundle::default());
+}
 
 impl ShowIntroScreen for GameIntroScreen {
     fn configure_ui<S, D, U>(&self, app: &mut App, preferences: &IntroPreferences<S, D, U>)
@@ -58,299 +89,26 @@ impl ShowIntroScreen for GameIntroScreen {
         D: IntroDuration,
         U: ShowIntroScreen,
     {
-        app.add_systems(Update,setup.run_if(is_running)).add_systems(OnExit(IntroState::Running),clean_up);
-    }
+        let egui = EguiIntroScreen::builder()
+        .label(LABEL.into())
+        .icon(bevy_egui::egui::include_image!(
+            "../assets/images/app_logo.png"
+        ))
+        .background(bevy_egui::egui::include_image!("../assets/images/blue_background.png"))
+        .build();
+
+    egui.configure_ui(app, preferences);
+
+ }
 }
 
+#[derive(Clone)]
+pub struct GameIntroScreen;
 
-#[derive(Component,Clone,Debug)]
-struct UiMarker;
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
+pub enum AppState {
+    #[default]
+    SplashScreen,
 
-fn setup(mut commands : Commands) {
-    // Camera
-    commands.spawn((Camera2dBundle::default(), IsDefaultUiCamera));
-
-
-    // root node
-    commands
-        .spawn((UiMarker,NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                justify_content: JustifyContent::SpaceBetween,
-                ..default()
-            },
-            ..default()
-        }))
-        .with_children(|parent| {
-            // left vertical fill (border)
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        width: Val::Px(200.),
-                        border: UiRect::all(Val::Px(2.)),
-                        ..default()
-                    },
-                    background_color: Color::srgb(0.65, 0.65, 0.65).into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    // left vertical fill (content)
-                    parent
-                        .spawn(NodeBundle {
-                            style: Style {
-                                width: Val::Percent(100.),
-                                flex_direction: FlexDirection::Column,
-                                padding: UiRect::all(Val::Px(5.)),
-                                row_gap: Val::Px(5.),
-                                ..default()
-                            },
-                            background_color: Color::srgb(0.15, 0.15, 0.15).into(),
-                            ..default()
-                        })
-                        .with_children(|parent| {
-                            // text
-                            parent.spawn((
-                                TextBundle::from_section(
-                                    "Text Example",
-                                    TextStyle {
-                           //             font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                        font_size: 30.0,
-                                        ..default()
-                                    },
-                                ),
-                                // Because this is a distinct label widget and
-                                // not button/list item text, this is necessary
-                                // for accessibility to treat the text accordingly.
-                                Label,
-                            ));
-                        });
-                });
-            // right vertical fill
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        flex_direction: FlexDirection::Column,
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
-                        width: Val::Px(200.),
-                        ..default()
-                    },
-                    background_color: Color::srgb(0.15, 0.15, 0.15).into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    // Title
-                    parent.spawn((
-                        TextBundle::from_section(
-                            "Scrolling list",
-                            TextStyle {
-                   //             font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                font_size: 25.,
-                                ..default()
-                            },
-                        ),
-                        Label,
-                    ));
-                    // List with hidden overflow
-                    parent
-                        .spawn(NodeBundle {
-                            style: Style {
-                                flex_direction: FlexDirection::Column,
-                                align_self: AlignSelf::Stretch,
-                                height: Val::Percent(50.),
-                                overflow: Overflow::clip_y(),
-                                ..default()
-                            },
-                            background_color: Color::srgb(0.10, 0.10, 0.10).into(),
-                            ..default()
-                        })
-                        .with_children(|parent| {
-                            // Moving panel
-                            parent
-                                .spawn((
-                                    NodeBundle {
-                                        style: Style {
-                                            flex_direction: FlexDirection::Column,
-                                            align_items: AlignItems::Center,
-                                            ..default()
-                                        },
-                                        ..default()
-                                    },
-                                ))
-                                .with_children(|parent| {
-                                    // List items
-                                    for i in 0..30 {
-                                        parent.spawn((
-                                            TextBundle::from_section(
-                                                format!("Item {i}"),
-                                                TextStyle {
-                                                   // font: asset_server
-                                                     //   .load("fonts/FiraSans-Bold.ttf"),
-                                                    ..default()
-                                                },
-                                            ),
-                                            Label,
-                                        ));
-                                    }
-                                });
-                        });
-                });
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        width: Val::Px(200.0),
-                        height: Val::Px(200.0),
-                        position_type: PositionType::Absolute,
-                        left: Val::Px(210.),
-                        bottom: Val::Px(10.),
-                        border: UiRect::all(Val::Px(20.)),
-                        ..default()
-                    },
-                    border_color: bevy::color::palettes::basic::LIME.into(),
-                    background_color: Color::srgb(0.4, 0.4, 1.).into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent.spawn(NodeBundle {
-                        style: Style {
-                            width: Val::Percent(100.0),
-                            height: Val::Percent(100.0),
-                            ..default()
-                        },
-                        background_color: Color::srgb(0.8, 0.8, 1.).into(),
-                        ..default()
-                    });
-                });
-            // render order test: reddest in the back, whitest in the front (flex center)
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        width: Val::Percent(100.0),
-                        height: Val::Percent(100.0),
-                        position_type: PositionType::Absolute,
-                        align_items: AlignItems::Center,
-                        justify_content: JustifyContent::Center,
-                        ..default()
-                    },
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent
-                        .spawn(NodeBundle {
-                            style: Style {
-                                width: Val::Px(100.0),
-                                height: Val::Px(100.0),
-                                ..default()
-                            },
-                            background_color: Color::srgb(1.0, 0.0, 0.).into(),
-                            ..default()
-                        })
-                        .with_children(|parent| {
-                            parent.spawn(NodeBundle {
-                                style: Style {
-                                    // Take the size of the parent node.
-                                    width: Val::Percent(100.0),
-                                    height: Val::Percent(100.0),
-                                    position_type: PositionType::Absolute,
-                                    left: Val::Px(20.),
-                                    bottom: Val::Px(20.),
-                                    ..default()
-                                },
-                                background_color: Color::srgb(1.0, 0.3, 0.3).into(),
-                                ..default()
-                            });
-                            parent.spawn(NodeBundle {
-                                style: Style {
-                                    width: Val::Percent(100.0),
-                                    height: Val::Percent(100.0),
-                                    position_type: PositionType::Absolute,
-                                    left: Val::Px(40.),
-                                    bottom: Val::Px(40.),
-                                    ..default()
-                                },
-                                background_color: Color::srgb(1.0, 0.5, 0.5).into(),
-                                ..default()
-                            });
-                            parent.spawn(NodeBundle {
-                                style: Style {
-                                    width: Val::Percent(100.0),
-                                    height: Val::Percent(100.0),
-                                    position_type: PositionType::Absolute,
-                                    left: Val::Px(60.),
-                                    bottom: Val::Px(60.),
-                                    ..default()
-                                },
-                                background_color: Color::srgb(1.0, 0.7, 0.7).into(),
-                                ..default()
-                            });
-                            // alpha test
-                            parent.spawn(NodeBundle {
-                                style: Style {
-                                    width: Val::Percent(100.0),
-                                    height: Val::Percent(100.0),
-                                    position_type: PositionType::Absolute,
-                                    left: Val::Px(80.),
-                                    bottom: Val::Px(80.),
-                                    ..default()
-                                },
-                                background_color: Color::srgba(1.0, 0.9, 0.9, 0.4).into(),
-                                ..default()
-                            });
-                        });
-                });
-            // bevy logo (flex center)
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        width: Val::Percent(100.0),
-                        position_type: PositionType::Absolute,
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::FlexStart,
-                        ..default()
-                    },
-                    ..default()
-                })
-                .with_children(|parent| {
-                    // bevy logo (image)
-                    // A `NodeBundle` is used to display the logo the image as an `ImageBundle` can't automatically
-                    // size itself with a child node present.
-                    parent
-                        .spawn((
-                            NodeBundle {
-                                style: Style {
-                                    width: Val::Px(500.0),
-                                    height: Val::Px(125.0),
-                                    margin: UiRect::top(Val::VMin(5.)),
-                                    ..default()
-                                },
-                                ..default()
-                            },
-//                            UiImage::new(asset_server.load("branding/bevy_logo_dark_big.png")),
-                        ))
-                        .with_children(|parent| {
-                            // alt text
-                            // This UI node takes up no space in the layout and the `Text` component is used by the accessibility module
-                            // and is not rendered.
-                            parent.spawn((
-                                NodeBundle {
-                                    style: Style {
-                                        display: Display::None,
-                                        ..Default::default()
-                                    },
-                                    ..Default::default()
-                                },
-                                Text::from_section("Bevy logo", TextStyle::default()),
-                            ));
-                        });
-                });
-        });
-
-}
-
-fn clean_up(mut commands : Commands,query : Query<Entity,With<UiMarker>>) {
-    // Should only be a single entity
-    for entity in &query {
-        commands.entity(entity).despawn_recursive();
-    }
+    GameMenu,
 }
